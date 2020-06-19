a9fb7f6fcb6a75a13a4254554563bc6ce3b6b934
Nachiket Mistry <nmistry@splunk.com>
2020-06-18 17:08:42 -0700

    feat: space out the poop

--- src/tests/poop.test.js
+++ src/tests/poop.test.js
@@ -2,5 +2,5 @@ const sayPoop = require('../lib/poop');
 
 test('says with poop', () => {
     const str = 'hello';
-    expect(sayPoop(str)).toBe(`💩${str}💩`);
+    expect(sayPoop(str)).toBe(`💩 ${str} 💩`);
 })
